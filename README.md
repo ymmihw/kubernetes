21a53561c80e718fddc8bdf74478cf9bfac5d5b9
wanghaoming <wanghaoming@ctfo.com>
2019-07-02 15:41:00 +0800

    add self-healing-with-spring-boot-actuator

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Module | Article
 --|--
 running-with-minikube | [Running Spring Boot Applications With Minikube](http://www.baeldung.com/spring-boot-minikube)
+self-healing-with-spring-boot-actuator | [Self-Healing Applications with Kubernetes and Spring Boot](https://www.baeldung.com/spring-boot-kubernetes-self-healing-apps)
---
+++ self-healing-with-spring-boot-actuator/readiness-example/src/test/java/com/ymmihw/kubernetes/SpringContextIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.ymmihw.kubernetes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.ymmihw.kubernetes.readiness.Application;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class SpringContextIntegrationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
